b79db5f4a916a8a68bedcbcc18b90c6aeca8202c
Enrico Prado Mazzochi <13022744650@campus.machado.mch>
2022-07-04 20:06:08 -0300

    first commit

---
+++ app/src/main/java/br/edu/ifsuldeminas/mch/tarefas2/domain/Task.java
@@ -0,0 +1,42 @@
+package br.edu.ifsuldeminas.mch.tarefas2.domain;
+
+import java.io.Serializable;
+
+public class Task implements Serializable {
+
+    private Integer id;
+    private String description;
+    private boolean active;
+
+    public Task(Integer id, String description){
+        this.id = id;
+        this.description = description;
+        active = true;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+
+    @Override
+    public String toString(){
+        return String.format("%s", description);
+    }
+}
